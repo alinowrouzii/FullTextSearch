50528733102a1b81f33810d5ad1b8865bb65eca6
alinowrouzii <alinowrouzii@gmail.com>
2022-02-22 18:11:28 +0330

    Add phone validator

--- models/Contact.js
+++ models/Contact.js
@@ -3,18 +3,24 @@ const mongoose = require('mongoose');
 const contactSchema = new mongoose.Schema({
     firstName: {
         type: String,
-        required: true,
+        required: [true, 'First name is required'],
         maxLength: 40,
     },
     lastName: {
         type: String,
-        required: true,
+        required: [true, 'Last name is required'],
         maxLength: 40,
     },
     phoneNumber: {
         type: String,
-        required: true,
+        required: [true, 'Phone number is required'],
         maxLength: 40,
+        validate: {
+            validator: function (v) {
+                return /^0?\d{10}$/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
     },
 }, { timestamps: true });
 
