538384cb1ef2cbc583f2bd41dd571b3c2dac81a4
alinowrouzii <alinowrouzii@gmail.com>
2022-02-23 17:53:57 +0330

    Add CRUD operation to redis Inverted Index

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose');
 const Contact = require('../models/Contact.js');
-const { addData } = require('./runners/index.js');
+const { addData, getData, deleteData, updateData } = require('./runners/index.js');
 
 exports.createContact = async (req, res) => {
     const { firstName, lastName, phoneNumber } = req.body;
@@ -36,25 +36,38 @@ exports.createContact = async (req, res) => {
 exports.updateContact = async (req, res) => {
     const { contactID, firstName, lastName, phoneNumber } = req.body;
 
-    if (!contactID || !firstName || !lastName || !phoneNumber) {
-        return res.status(400).json({ status: 'fail', detail: 'contactID && firstName and lastName and phoneNumber are required' });
+    if (!contactID || (!firstName && !lastName && !phoneNumber)) {
+        return res.status(400).json({ status: 'fail', detail: 'contactID && (firstName or lastName or phoneNumber) are required' });
     }
 
     try {
+        if (!mongoose.Types.ObjectId.isValid(contactID)) {
+            return res.status(400).json({ status: 'fail', detail: 'Invalid Contact ID' });
+        }
+
         const id = mongoose.Types.ObjectId(contactID);
 
         const query = { _id: id };
-        const updatedFields = { firstName, lastName, phoneNumber }
-        const contact = await Contact.findOneAndUpdate(query, updatedFields, {
-            new: true
-        });
+        const contact = await Contact.findOne(query);
+        const oldContact = contact.toObject();
+
+        if (firstName) {
+            contact.firstName = firstName
+        }
+        if (lastName) {
+            contact.lastName = lastName
+        }
+        if (phoneNumber) {
+            contact.phoneNumber = phoneNumber
+        }
+        await contact.save();
+
+        const redisClient = req.redis;
+        await updateData(oldContact, contact, redisClient);
 
         return res.status(200).json({ status: 'success', contact: contact.toObject() });
     } catch (e) {
 
-        if (String(e).includes("BSONTypeError")) {
-            return res.status(400).json({ status: 'fail', detail: 'Invalid Contact ID' });
-        }
         return res.status(404).json({ status: 'fail', detail: 'Contact not found' });
     }
 
@@ -62,24 +75,50 @@ exports.updateContact = async (req, res) => {
 
 exports.getContact = async (req, res) => {
 
+    const textToSearch = req.query.text;
+
+    if (!textToSearch) {
+        return res.status(400).json({ status: 'fail', detail: 'Bad request' });
+    }
+
+    const redisClient = req.redis;
+    let fetchedIDS = await getData(textToSearch, redisClient)
+
+    const fetchedContacts = await Contact.find({
+        '_id': {
+            $in: fetchedIDS
+        }
+    });
+
+    return res.status(200).json({ status: 'success', contacts: fetchedContacts });
+
 }
 
 exports.deleteContact = async (req, res) => {
     const { contact_id: contactID } = req.params;
 
     try {
+
+        if (!mongoose.Types.ObjectId.isValid(contactID)) {
+            return res.status(400).json({ status: 'fail', detail: 'Invalid Contact ID' });
+        }
+
         const id = mongoose.Types.ObjectId(contactID);
 
         const query = { _id: id };
         const obj = await Contact.findOneAndDelete(query);
+
         if (!obj) {
             return res.status(404).json({ status: 'fail', detail: 'Contact not found' });
         }
 
+        const redisClient = req.redis;
+        await deleteData(obj, redisClient);
+
         return res.status(200).json({ status: 'success', detail: 'Contact deleted successfully' });
     } catch (e) {
         // This error is because of ObjectId casting str to ObjectId
-        return res.status(400).json({ status: 'fail', detail: 'Invalid Contact ID' });
+        return res.status(400).json({ status: 'fail', detail: 'Bad request' });
     }
 
 }
--- controllers/runners/index.js
+++ controllers/runners/index.js
@@ -1,27 +1,51 @@
+const getData = async (query, redisClient) => {
 
+    const data_to_fetch = query.trim().split(" ")
 
-exports.getData = async (query, redisClient) => {
-
-
+    const fetchedIDS = await redisClient.SINTER(data_to_fetch)
+    return fetchedIDS
 }
 
-
-exports.addData = async (data, redisClient) => {
-    const {firstName, lastName, phoneNumber, id: docID} = data;
+const addData = async (data, redisClient) => {
+    const { firstName, lastName, phoneNumber, _id: docID } = data;
 
     let data_to_add = String(firstName + " " + lastName + " " + phoneNumber);
     data_to_add = data_to_add.trim().split(" ")
 
-    console.log(data_to_add)
-    // TODO: add all prefex of each word to set
     for (const word of data_to_add) {
-        redisClient.sAdd(word, docID)
-    }
 
-
-    for (const word of data_to_add) {
-        const names = await redisClient.sMembers(word)
-        console.log(word,names)
-        console.log("=====")
+        // add all prefix of word to our set.
+        // we store all prefix of words that have more than or equal of three chars
+        for (let endIndex = 3; endIndex <= word.length; endIndex++) {
+            const wordToAdd = word.substring(0, endIndex)
+            redisClient.sAdd(wordToAdd, docID)
+        }
     }
 }
+
+const deleteData = async (oldData, redisClient) => {
+
+    const { firstName, lastName, phoneNumber, _id: docID } = oldData;
+
+    let dataToRemove = String(firstName + " " + lastName + " " + phoneNumber);
+    dataToRemove = dataToRemove.trim().split(" ")
+
+    for (const word of dataToRemove) {
+        // Similar to addData, remove all prefix of stored data
+        for (let endIndex = 3; endIndex <= word.length; endIndex++) {
+            const wordToRemove = word.substring(0, endIndex)
+            redisClient.sRem(wordToRemove, docID)
+        }
+    }
+}
+
+
+const updateData = async (oldData, newData, redisClient) => {
+    await deleteData(oldData, redisClient);
+    await addData(newData, redisClient);
+}
+
+exports.addData = addData;
+exports.getData = getData;
+exports.deleteData = deleteData;
+exports.updateData = updateData;
